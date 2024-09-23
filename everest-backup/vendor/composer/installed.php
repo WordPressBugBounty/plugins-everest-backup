350122e23275fb3b201f186ef68c944c8afb367b
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 11:39:02 +0000

    2.2.12

---
+++ everest-backup/vendor/composer/installed.php
@@ -0,0 +1,52 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '650421dade0049633db5358266e8256556a5f30e',
+        'name' => 'everest-backup/everest-backup',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'everest-backup/everest-backup' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '650421dade0049633db5358266e8256556a5f30e',
+            'dev_requirement' => false,
+        ),
+        'nelexa/zip' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nelexa/zip',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '330c724ce6980a7ae0e00e0e7072893282644c6f',
+            'dev_requirement' => false,
+        ),
+        'psr/http-message' => array(
+            'pretty_version' => '2.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-message',
+            'aliases' => array(),
+            'reference' => '402d35bcb92c70c026d1a6a9883f06b2ead23d71',
+            'dev_requirement' => false,
+        ),
+        'symfony/finder' => array(
+            'pretty_version' => 'v6.4.11',
+            'version' => '6.4.11.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/finder',
+            'aliases' => array(),
+            'reference' => 'd7eb6daf8cd7e9ac4976e9576b32042ef7253453',
+            'dev_requirement' => false,
+        ),
+    ),
+);
